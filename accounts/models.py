f9ebc7bb871a78da1a47afb5397a2ac46c4e8994
suabae <bsa0322@ewhain.net>
2023-01-06 16:59:49 +0900

    [add] basemodel

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
+
 # Create your models here.
+
+class BaseModel(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+    update_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        abstract = True
