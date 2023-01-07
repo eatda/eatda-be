2f237387382e88122b152acb6bcb958e5cb49522
suabae <bsa0322@ewhain.net>
2023-01-07 15:19:15 +0900

    [chore] delete docker setting

--- config/docker/entrypoint.sh
+++ config/docker/entrypoint.sh
@@ -13,5 +13,5 @@ fi
 
 autossh -i ${SSH_PEM_KEY} -M 33306 -4fNL ${DATABASE_PORT}:${RDS_HOST}:${DATABASE_PORT} ${SSH_USER}@${SSH_HOST}
 
-#python manage.py migrate
+python manage.py migrate
 python manage.py runserver 0.0.0.0:8000
