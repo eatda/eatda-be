eb5f50e94874aa9ad9e07f267113c7e3196ea7d3
suabae <bsa0322@ewhain.net>
2023-01-07 23:15:32 +0900

    [add] diet model

--- eatda_be/settings/base.py
+++ eatda_be/settings/base.py
@@ -14,7 +14,7 @@ import os
 import environ
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 env = environ.Env(
     DEBUG=(bool, False)
 )
--- eatda_be/urls.py
+++ eatda_be/urls.py
@@ -15,6 +15,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -22,3 +24,8 @@ urlpatterns = [
     path('users/', include('users.urls')),
     path('diets/', include('diets.urls'))
 ]
+
+# 개발 환경 미디어 세팅
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
