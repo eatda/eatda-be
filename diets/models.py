5ff5b577f0faf09c5733c5f9aad6403e264f85ee
suabae <bsa0322@ewhain.net>
2023-01-09 21:54:53 +0900

    [add] separate main & side table

--- diets/models.py
+++ diets/models.py
@@ -18,7 +18,7 @@ class Filter(BaseModel):
     filter_category = models.ForeignKey(FilterCategory, on_delete=models.CASCADE)  # 필터 카테고리 id
 
 
-# 식단 테이블
+# 주 식단 테이블
 class Data(BaseModel):
     name = models.JSONField(default=dict)  # 음식 이름
     image = models.ImageField(upload_to='images/', default='default.jpg')  # 음식 이미지
@@ -31,7 +31,25 @@ class Data(BaseModel):
     recipe = models.TextField(default='')  # 레시피
     tip = models.TextField(default='')  # 건강 비결
     user = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, default=None)  # 작성자
-    is_side = models.BooleanField(default=False)  # 사이드 메뉴 여부
+    type = models.ForeignKey(Filter, on_delete=models.SET_NULL, null=True, related_name='type', default=None)  # 음식 종류
+    flavor = models.ForeignKey(Filter, on_delete=models.SET_NULL, null=True, related_name='flavor', default=None)  # 맛
+    carbohydrate_type = models.ForeignKey(Filter, on_delete=models.SET_NULL,
+                                          null=True, related_name='carbohydrate_type', default=None)  # 현미 종류
+
+
+# 사이드 식단 테이블
+class SideData(BaseModel):
+    name = models.JSONField(default=dict)  # 음식 이름
+    image = models.ImageField(upload_to='images/', default='default.jpg')  # 음식 이미지
+    carbohydrate = models.IntegerField(default=0)  # 탄수화물
+    protein = models.IntegerField(default=0)  # 단백질
+    province = models.IntegerField(default=0)  # 지방
+    salt = models.IntegerField(default=0)  # 나트륨
+    total_calorie = models.IntegerField(default=0)  # 총 칼로리
+    ingredient = models.JSONField(default=list)  # 재료
+    recipe = models.TextField(default='')  # 레시피
+    tip = models.TextField(default='')  # 건강 비결
+    user = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, default=None)  # 작성자
     type = models.ForeignKey(Filter, on_delete=models.SET_NULL, null=True, related_name='type', default=None)  # 음식 종류
     flavor = models.ForeignKey(Filter, on_delete=models.SET_NULL, null=True, related_name='flavor', default=None)  # 맛
     carbohydrate_type = models.ForeignKey(Filter, on_delete=models.SET_NULL,
@@ -41,7 +59,7 @@ class Data(BaseModel):
 # 사이드 메뉴 관계 테이블
 class MainSide(BaseModel):
     main = models.ForeignKey(Data, on_delete=models.CASCADE, related_name='main')  # 메인 메뉴
-    side = models.ForeignKey(Data, on_delete=models.CASCADE, related_name='side')  # 사이드 메뉴
+    side = models.ForeignKey(SideData, on_delete=models.CASCADE, related_name='side')  # 사이드 메뉴
 
 
 # 알러지 테이블
