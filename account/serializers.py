4d275dd2632e7b075aff6f8c07ed28017f684e9b
suabae <bsa0322@ewhain.net>
2023-01-12 03:22:39 +0900

    [feat] sign-up(ing) sign-in auth

---
+++ account/serializers.py
@@ -0,0 +1,83 @@
+from rest_framework import serializers
+from django.contrib.auth import get_user_model
+from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
+
+User = get_user_model()
+
+
+# 회원 가입
+class RegisterSerializer(serializers.ModelSerializer):
+    id = serializers.UUIDField(read_only=True)
+
+    social_id = serializers.CharField(
+        required=True,
+        write_only=True,
+    )
+
+    email = serializers.EmailField(
+        required=True,
+        write_only=True,
+    )
+
+    class Meta:
+        model = User
+        fields = ['id', 'social_id', 'email']
+
+    def save(self, validated_data):
+        social_id = validated_data.get('social_id')
+        email = validated_data.get('email')
+        user = User(
+            social_id=social_id,
+            email=email
+        )
+        user.save()
+        return user
+
+    def validate(self, data):
+        social_id = data.get('social_id', None)
+        email = data.get('email', None)
+
+        # 이미 존재하는 계정인지 확인
+        if User.objects.filter(social_id=social_id, email=email).exists():
+            raise serializers.ValidationError("User already exists")
+        return data
+
+
+# 로그인
+class LoginSerializer(serializers.ModelSerializer):
+    id = serializers.UUIDField(read_only=True)
+
+    social_id = serializers.CharField(
+        required=True,
+        write_only=True,
+    )
+
+    email = serializers.EmailField(
+        required=True,
+        write_only=True,
+    )
+
+    class Meta:
+        model = User
+        fields = ['id', 'social_id', 'email']
+
+    def validate(self, data):
+        social_id = data.get('social_id', None)
+        email = data.get('email', None)
+
+        if User.objects.filter(social_id=social_id, email=email).exists():
+            user = User.objects.get(social_id=social_id, email=email)
+        else:
+            raise serializers.ValidationError("user account not exist")
+
+        token = TokenObtainPairSerializer.get_token(user)  # 토큰 생성
+        refresh_token = str(token)  # refresh 토큰
+        access_token = str(token.access_token)  # access 토큰
+
+        data = {
+            'user': user,
+            'refresh_token': refresh_token,
+            'access_token': access_token,
+        }
+
+        return data
--- account/urls.py
+++ account/urls.py
@@ -1,3 +1,8 @@
 from django.urls import path
+from account.views import RegisterView, LoginView, AuthView
 
-urlpatterns = []
+urlpatterns = [
+    path('register/', RegisterView.as_view()),  # 회원 가입
+    path('login/', LoginView.as_view()),  # 로그인
+    path('auth/', AuthView.as_view()),  # 인가 (사용자 확인)
+]
