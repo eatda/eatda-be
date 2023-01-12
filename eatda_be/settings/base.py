9570d969e15e92a898f1c14383bfa720712f4bcc
suabae <bsa0322@ewhain.net>
2023-01-12 14:35:32 +0900

    [fix] base dir

--- eatda_be/settings/base.py
+++ eatda_be/settings/base.py
@@ -17,7 +17,7 @@ import environ
 from datetime import timedelta
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 env = environ.Env(
     DEBUG=(bool, False)
 )
