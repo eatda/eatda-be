d55a662842ff0771b9773143ce0f6b7b112de434
jianny-lee <jianny.lee96@gmail.com>
2023-01-13 19:07:13 +0900

    [chore] edit UserCharacterView class in views.py file

--- user/views.py
+++ user/views.py
@@ -6,16 +6,29 @@ from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
-from user.models import Character
+from user.models import Character, Info
 from user.serializers import CharacterSerializer
 
 
 class UserCharacterView(APIView):
     def get(self, request):
         try:
+            # 그룹 내 유저들의 캐릭터 ID 받아오기
             group_id = request.GET.get('groupid')
-            characterList = Character.objects.exclude(id=group_id)
-            serializer = CharacterSerializer(characterList, many=True,context={'request': request})
+            groupList = Info.objects.filter(group=group_id).values() #해당하는 그룹id의 유저들 get
+            selected_character = list()
+
+            for users in groupList:
+                selected_character.append(users['character_id'])
+            selected_character.sort()
+
+            # 선택된 캐릭터 제외하기
+            allCharacterList = Character.objects.all()
+
+            for select in selected_character:
+                unselected = allCharacterList.exclude(id=select)
+
+            serializer = CharacterSerializer(unselected, many=True, context={'request': request})
             return Response(serializer.data, status=status.HTTP_200_OK)
 
         except:
