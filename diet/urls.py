17a2946cb905c754933614e35563e576e50d3baf
suabae <bsa0322@ewhain.net>
2023-01-15 03:26:14 +0900

    [add] get filter list

--- diet/urls.py
+++ diet/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
-from diet.views import DietAllergyView
+from diet.views import DietAllergyView, FilterView
 
 urlpatterns = [
-    path('allergy', DietAllergyView.as_view())
+    path('allergy', DietAllergyView.as_view()),
+    path('filter/', FilterView.as_view())
 ]
