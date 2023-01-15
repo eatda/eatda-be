ac10b05514f94408a9cc69a8171ba2f94e2540c0
jianny-lee <jianny.lee96@gmail.com>
2023-01-15 16:47:07 +0900

    [chore] add '/' at end of urls

--- diet/urls.py
+++ diet/urls.py
@@ -3,5 +3,5 @@ from django.urls import path
 from diet.views import DietAllergyList
 
 urlpatterns = [
-    path('allergy', DietAllergyList.as_view())
+    path('allergy/', DietAllergyList.as_view())
 ]
