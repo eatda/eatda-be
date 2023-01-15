e5efeb4ccc50c08799ec1501233ec2f6b8c8986f
jianny-lee <jianny.lee96@gmail.com>
2023-01-15 16:47:34 +0900

    [feat] user info and mypage api

--- user/urls.py
+++ user/urls.py
@@ -2,8 +2,9 @@ from django.conf.urls.static import static
 from django.urls import path
 
 from eatda_be import settings
-from user.views import UserCharacterView
+from user.views import UserCharacterView, UserInfoDetailView
 
 urlpatterns = [
-    path('character', UserCharacterView.as_view())
+    path('info/', UserInfoDetailView.as_view()),  # 유저 정보
+    path('character/', UserCharacterView.as_view())  # 캐릭터 정보
 ]
--- user/views.py
+++ user/views.py
@@ -1,13 +1,64 @@
-from django.shortcuts import render
-
 # 이미 선택된 캐릭터 제외하고 캐릭터 리스트 보내주는 api
 
-from rest_framework import status
+from rest_framework import status, exceptions
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
-from user.models import Character, Info
-from user.serializers import CharacterSerializer
+from account.views import AuthView
+from diet.models import DietAllergy
+from user.models import Character, Info, UserAllergy
+from user.serializers import CharacterSerializer, InfoSerializer, UserAllergySerializer
+
+
+class UserInfoDetailView(APIView):
+    def get(self, request):
+        # 인가확인
+        if AuthView.get(self, request).status_code is status.HTTP_200_OK:
+            # 접속한 유저 정보 가져오기
+            login_user = AuthView.get(self, request).data
+            login_user_detail = Info.objects.get(user_id=login_user['user_id'])
+
+            if login_user_detail is not None:
+                try:
+                    allergy_list = UserAllergy.objects.filter(user_id=login_user['user_id'])
+
+                    info_serializer = InfoSerializer(login_user_detail, context={'request': request})
+                    allergy_serializer = UserAllergySerializer(allergy_list, many=True, context={"request": request})
+
+                    # 유저 알러지 리스트 가져오기
+                    allergy_filter = []
+                    for i in range(len(allergy_list)):
+                        allergy=DietAllergy.objects.filter(id=allergy_serializer.data[i].get('allergy_id'))
+                        allergy_data = {
+                            'id': allergy.get().id,
+                            'name': allergy.get().name
+                        }
+                        allergy_filter.append(allergy_data)
+
+                    data = {
+                        'name': info_serializer.data['name'],
+                        'character': info_serializer.data['character'],
+                        'height': info_serializer.data['height'],
+                        'weight': info_serializer.data['weight'],
+                        'gender': info_serializer.data['gender'],
+                        'is_diabetes': info_serializer.data['is_diabetes'],
+                        'group_id': info_serializer.data['group_id'],
+                        'allergy': allergy_filter
+                    }
+
+                    return Response(data, status=status.HTTP_200_OK)
+
+                except Exception as e:
+                    return Response({"error": str(e)}, status=status.HTTP_400_BAD_REQUEST)
+
+        elif AuthView.get(self, request).status_code is status.HTTP_400_BAD_REQUEST:
+            raise exceptions.ValidationError(detail='Please login for getting user info')
+
+        elif AuthView.get(self, request).status_code is status.HTTP_401_UNAUTHORIZED:
+            raise exceptions.ValidationError(detail='token is expired')
+
+        elif AuthView.get(self, request).status_code is status.HTTP_403_FORBIDDEN:
+            raise exceptions.ValidationError(detail='Please login again')
 
 
 class UserCharacterView(APIView):
@@ -32,4 +83,4 @@ class UserCharacterView(APIView):
             return Response(serializer.data, status=status.HTTP_200_OK)
 
         except Exception as e:
-            return Response({"error": e}, status=status.HTTP_400_BAD_REQUEST)
+            return Response({"error": str(e)}, status=status.HTTP_400_BAD_REQUEST)
