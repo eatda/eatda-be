3e7757ec594b071adaad47bdece5b23699ce0e27
suabae <bsa0322@ewhain.net>
2023-01-17 03:06:08 +0900

    [fix] separate diabetes serializer

--- user/serializers.py
+++ user/serializers.py
@@ -21,15 +21,54 @@ class InfoAuthSerializer(serializers.ModelSerializer):
         fields = ['user_id', 'is_diabetes', 'character']
 
 
-# 유저 전체 정보
+# 기본 유저 정보 (당뇨인 & 비당뇨인)
+class InfoBasicSerializer(serializers.ModelSerializer):
+    user_id = serializers.UUIDField(read_only=True)
+    character_id = serializers.IntegerField(write_only=True)
+    character = serializers.SerializerMethodField(read_only=True)
+    group_id = serializers.IntegerField(required=True)
+
+    def get_character(self, obj):
+        request = self.context.get('request')
+        if obj.character.image:
+            return request.build_absolute_uri(obj.character.image.url)
+        return None
+
+    class Meta:
+        model = Info
+        fields = ['user_id', 'name', 'character_id', 'character', 'is_diabetes', 'group_id']
+
+    def save(self, validated_data):
+        social_id = validated_data.get('social_id')
+        email = validated_data.get('email')
+        user_id = User.objects.get(social_id=social_id, email=email).id
+        name = validated_data.get('name')
+        character_id = validated_data.get('character_id')
+        is_diabetes = validated_data.get('is_diabetes')
+        group_id = validated_data.get('group_id')
+
+        info = Info(
+            user_id=user_id,
+            name=name,
+            character_id=character_id,
+            is_diabetes=is_diabetes,
+            group_id=group_id
+        )
+        # 유저 정보 저장
+        info.save()
+        return info
+
+
+# 유저 전체 정보 (당뇨인)
 class InfoSerializer(serializers.ModelSerializer):
     user_id = serializers.UUIDField(read_only=True)
     character_id = serializers.IntegerField(write_only=True)
     character = serializers.SerializerMethodField(read_only=True)
-    height = serializers.FloatField(required=False)
-    weight = serializers.FloatField(required=False)
-    gender = serializers.CharField(required=False)
-    activity = serializers.IntegerField(required=False)
+    height = serializers.FloatField(required=True)
+    weight = serializers.FloatField(required=True)
+    gender = serializers.CharField(required=True)
+    activity = serializers.IntegerField(required=True)
+    group_id = serializers.IntegerField(required=True)
 
     def get_character(self, obj):
         request = self.context.get('request')
