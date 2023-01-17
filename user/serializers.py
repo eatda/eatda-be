441bf2fc1f766b6de126024ec7a4aa235a07284e
suabae <bsa0322@ewhain.net>
2023-01-17 20:54:39 +0900

    [fix] delete character image in user serializer

--- user/serializers.py
+++ user/serializers.py
@@ -8,13 +8,6 @@ User = get_user_model()
 # 유저 ID 정보
 class InfoAuthSerializer(serializers.ModelSerializer):
     user_id = serializers.UUIDField(read_only=True)
-    character = serializers.SerializerMethodField(read_only=True)
-
-    def get_character(self, obj):
-        request = self.context.get('request')
-        if obj.character.image:
-            return request.build_absolute_uri(obj.character.image.url)
-        return None
 
     class Meta:
         model = Info
@@ -24,33 +17,26 @@ class InfoAuthSerializer(serializers.ModelSerializer):
 # 기본 유저 정보 (당뇨인 & 비당뇨인)
 class InfoBasicSerializer(serializers.ModelSerializer):
     user_id = serializers.UUIDField(read_only=True)
-    character_id = serializers.IntegerField(write_only=True)
-    character = serializers.SerializerMethodField(read_only=True)
     group_id = serializers.IntegerField(required=True)
-
-    def get_character(self, obj):
-        request = self.context.get('request')
-        if obj.character.image:
-            return request.build_absolute_uri(obj.character.image.url)
-        return None
+    character = serializers.IntegerField(required=True)
 
     class Meta:
         model = Info
-        fields = ['user_id', 'name', 'character_id', 'character', 'is_diabetes', 'group_id']
+        fields = ['user_id', 'name', 'character', 'is_diabetes', 'group_id']
 
     def save(self, validated_data):
         social_id = validated_data.get('social_id')
         email = validated_data.get('email')
         user_id = User.objects.get(social_id=social_id, email=email).id
         name = validated_data.get('name')
-        character_id = validated_data.get('character_id')
+        character = validated_data.get('character')
         is_diabetes = validated_data.get('is_diabetes')
         group_id = validated_data.get('group_id')
 
         info = Info(
             user_id=user_id,
             name=name,
-            character_id=character_id,
+            character=character,
             is_diabetes=is_diabetes,
             group_id=group_id
         )
@@ -62,23 +48,16 @@ class InfoBasicSerializer(serializers.ModelSerializer):
 # 유저 전체 정보 (당뇨인)
 class InfoSerializer(serializers.ModelSerializer):
     user_id = serializers.UUIDField(read_only=True)
-    character_id = serializers.IntegerField(write_only=True)
-    character = serializers.SerializerMethodField(read_only=True)
+    character = serializers.IntegerField(required=True)
     height = serializers.FloatField(required=True)
     weight = serializers.FloatField(required=True)
     gender = serializers.CharField(required=True)
     activity = serializers.IntegerField(required=True)
     group_id = serializers.IntegerField(required=True)
 
-    def get_character(self, obj):
-        request = self.context.get('request')
-        if obj.character.image:
-            return request.build_absolute_uri(obj.character.image.url)
-        return None
-
     class Meta:
         model = Info
-        fields = ['user_id', 'name', 'character_id', 'character', 'height', 'weight', 'gender', 'is_diabetes',
+        fields = ['user_id', 'name', 'character', 'height', 'weight', 'gender', 'is_diabetes',
                   'activity', 'group_id']
 
     def save(self, validated_data):
@@ -86,7 +65,7 @@ class InfoSerializer(serializers.ModelSerializer):
         email = validated_data.get('email')
         user_id = User.objects.get(social_id=social_id, email=email).id
         name = validated_data.get('name')
-        character_id = validated_data.get('character_id')
+        character = validated_data.get('character')
         height = validated_data.get('height')
         weight = validated_data.get('weight')
         gender = validated_data.get('gender')
@@ -97,7 +76,7 @@ class InfoSerializer(serializers.ModelSerializer):
         info = Info(
             user_id=user_id,
             name=name,
-            character_id=character_id,
+            character=character,
             height=height,
             weight=weight,
             gender=gender,
