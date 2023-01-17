85df51ed3f669beeb90f269a2022ccfdaa4655ec
suabae <bsa0322@ewhain.net>
2023-01-18 02:48:55 +0900

    [add] like info

--- user/views.py
+++ user/views.py
@@ -3,15 +3,19 @@ import base64
 import codecs
 import uuid
 
+from django.shortcuts import get_object_or_404
 from rest_framework import status, exceptions
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
 from account.views import AuthView
 from diet.serializers import DietSimpleSerializer
-from user.models import Character, Info, Group, UserAllergy, BloodSugarLevel
+from user.models import Character, Info, Group, UserAllergy, BloodSugarLevel, Like
 from user.serializers import CharacterSerializer, GroupSerializer, InfoSerializer, UserAllergySerializer, \
     BloodSerializer
+
+
+from datetime import datetime
 
 
 class UserInfoDetailView(APIView):
@@ -140,6 +144,12 @@ class UserGroupView(APIView):
 
 # 홈 화면 api
 class UserHomeView(APIView):
+    def get_like_character(self, like_list):
+        who_liked = []
+        for like in like_list:
+            who_liked.append(like.user.character)
+        return who_liked
+
     def get(self, request):
         # 인가확인
         if AuthView.get(self, request).status_code is not status.HTTP_200_OK:
@@ -147,24 +157,34 @@ class UserHomeView(APIView):
 
         # 접속한 유저 정보 가져오기
         user_id = AuthView.get(self, request).data['user_id']
+        user = get_object_or_404(Info, user_id=user_id)
 
         # 현재 날짜 가져오기
-        # date = datetime.now().date()
-        date = "2023-01-15"
+        date = datetime.now().date()
 
-        # 오늘의 식단 & 혈당량 정보 리스트로 가져오기
+        # 오늘의 식단 & 혈당량 & 좋아요 정보 리스트로 가져오기 (해당 그룹에 대한)
         try:
-            diet_blood_list = BloodSugarLevel.objects.filter(user_id=user_id, created_at__contains=date)
+            diet_blood_list = BloodSugarLevel.objects.filter(user_id__group=user.group_id, created_at__contains=date)
+            like_list = Like.objects.filter(user_id__group=user.group_id, created_at__contains=date)
         except Exception as e:
             return Response({"error": str(e)}, status=status.HTTP_400_BAD_REQUEST)
 
-        # 아침, 점심, 저녁 마다 식단 & 혈당량 정보 얻어오기
+        # 아침, 점심, 저녁 마다 식단 & 혈당량 & 좋아요 정보 얻어오기
         diet = [{}, {}, {}]
         blood_sugar_level = [{}, {}, {}]
-        for timeline in range(0, 3):
+        for timeline in [timeline[0] for timeline in BloodSugarLevel.timeline.field.choices]:
             try:
                 blood_detail = diet_blood_list.get(timeline=timeline)  # 식후 혈당량
                 diet_detail = blood_detail.diet  # 식단
+                try:
+                    diet_like = like_list.filter(target=0, timeline=timeline)  # 식단 좋아요
+                except:
+                    diet_like = []
+                try:
+                    blood_like = like_list.filter(target=1, timeline=timeline)  # 식후 혈당량 좋아요
+                except:
+                    blood_like = []
+
                 try:
                     serializer = DietSimpleSerializer(diet_detail, context={'request': request})
                 except Exception as e:
@@ -172,6 +192,14 @@ class UserHomeView(APIView):
                 diet[timeline]["is_exist"] = True
                 diet[timeline]["data"] = serializer.data
                 diet[timeline]["data"]["timeline"] = timeline
+                try:
+                    diet[timeline]["is_me_liked"] = True if diet_like.get(user_id=user.user_id) else False
+                except:
+                    diet[timeline]["is_me_liked"] = False
+                try:
+                    diet[timeline]["who_liked"] = self.get_like_character(diet_like)
+                except Exception as e:
+                    return Response(str(e))
 
                 if blood_detail.time is None:  # 식후 혈당량 없다면
                     blood_sugar_level[timeline]["is_exist"] = False
@@ -179,8 +207,14 @@ class UserHomeView(APIView):
                     blood_sugar_level[timeline]["is_exist"] = True
                     serializer = BloodSerializer(blood_detail)
                     blood_sugar_level[timeline]["data"] = serializer.data
+                    try:
+                        blood_sugar_level[timeline]["is_me_liked"] = True if blood_like.get(user_id=user.user_id) else False
+                    except:
+                        blood_sugar_level[timeline]["is_me_liked"] = False
+                    blood_sugar_level[timeline]["who_liked"] = self.get_like_character(blood_like)
             except:
                 diet[timeline]["is_exist"] = blood_sugar_level[timeline]["is_exist"] = False
+
         res_data = {
             "diet": diet,
             "blood_sugar_level": blood_sugar_level
