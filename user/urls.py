00909695524f4fc74f80ff4229c080b1dabd62c6
jianny-lee <jianny.lee96@gmail.com>
2023-01-18 19:02:44 +0900

    [feat] add url of ourpick api

--- user/urls.py
+++ user/urls.py
@@ -2,11 +2,12 @@ from django.conf.urls.static import static
 from django.urls import path
 
 from eatda_be import settings
-from user.views import UserCharacterView, UserGroupView, UserInfoDetailView, UserHomeView
+from user.views import UserCharacterView, UserGroupView, UserInfoDetailView, UserHomeView, OurPickView
 
 urlpatterns = [
     path('info/', UserInfoDetailView.as_view()),  # 유저 정보
     path('character/', UserCharacterView.as_view()),  # 캐릭터 정보
     path('group/code/', UserGroupView.as_view()),  # 그룹 정보
-    path('home/', UserHomeView.as_view())  # 홈
+    path('home/', UserHomeView.as_view()),  # 홈
+    path('diet/like/', OurPickView.as_view())  # OurPick
 ]
